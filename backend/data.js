54b38b2acdd1466e19fa2f6863dd0ce0cb4ec43d
Emil Schlichter <emilschlichter@gmail.com>
2023-09-07 07:51:05 +0200

    Make folders and create data

---
+++ backend/data.js
@@ -0,0 +1,144 @@
+[{
+    "id": "02",
+    "name": "MØ",
+    "birthdate": "1988-08-13",
+    "activeSince": "2007-2012 (as Karen Marie Ørsted), 2012-present (as MØ)",
+    "genres": [
+      "Pop",
+      "Electropop",
+      "Indie Pop"
+    ],
+    "labels": [
+      "Sony Music",
+      "Chess Club",
+      "RCA Records"
+    ],
+    "website": "https://www.momomoyouth.com/",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/M%C3%98-Profile.jpg/400px-M%C3%98-Profile.jpg",
+    "roles": [
+      "Singer",
+      "Songwriter"
+    ],
+    "shortDescription": "Acclaimed Danish pop sensation",
+    "favorites": true
+  },
+  {
+    "id": "03",
+    "name": "Rasmus Seebach",
+    "birthdate": "1980-03-28",
+    "activeSince": "2009-present",
+    "genres": [
+      "Pop",
+      "Rock",
+      "Dance"
+    ],
+    "labels": [
+      "ArtPeople",
+      "Universal Music"
+    ],
+    "website": "https://rasmusseebach.dk/",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Rasmus_Seebach_2009.jpg/400px-Rasmus_Seebach_2009.jpg",
+    "roles": [
+      "Singer",
+      "Songwriter"
+    ],
+    "shortDescription": "Danish pop sensation and prolific songwriter",
+    "favorites": true
+  },
+{
+    "id": "04",
+    "name": "Lukas Graham",
+    "birthdate": "1988-09-18",
+    "activeSince": "2011-present",
+    "genres": [
+      "Pop",
+      "Soul",
+      "R&B"
+    ],
+    "labels": [
+      "Copenhagen Records",
+      "Warner Bros. Records"
+    ],
+    "website": "https://www.lukasgraham.com/",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Lukas_Graham_-_2016_SXSW.jpg/400px-Lukas_Graham_-_2016_SXSW.jpg",
+    "roles": [
+      "Singer",
+      "Songwriter"
+    ],
+    "shortDescription": "Danish band known for their soulful pop hits",
+    "favorites": true
+  },
+  {
+    "id": "05",
+    "name": "Oh Land",
+    "birthdate": "1985-05-02",
+    "activeSince": "2008-present",
+    "genres": [
+      "Indie Pop",
+      "Electronic",
+      "Synth-pop"
+    ],
+    "labels": [
+      "Fake Diamond Records",
+      "A:larm Music"
+    ],
+    "website": "https://www.ohlandmusic.com/",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Oh_Land%2C_Vevo_Halloween_2015_%28cropped%29.jpg/400px-Oh_Land%2C_Vevo_Halloween_2015_%28cropped%29.jpg",
+    "roles": [
+      "Singer",
+      "Songwriter"
+    ],
+    "shortDescription": "Danish singer-songwriter and electronic music artist",
+    "favorites": false
+  },
+  {
+    "id": "06",
+    "name": "Medina",
+    "birthdate": "1982-11-03",
+    "activeSince": "2007-present",
+    "genres": [
+      "Pop",
+      "Electropop",
+      "Dance"
+    ],
+    "labels": [
+      "Labelmade",
+      "EMI Music"
+    ],
+    "website": "https://medinaofficial.dk/",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Medina_at_Danish_Music_Awards_2009_%28cropped%29.jpg/400px-Medina_at_Danish_Music_Awards_2009_%28cropped%29.jpg",
+    "roles": [
+      "Singer",
+      "Songwriter"
+    ],
+    "shortDescription": "Danish pop sensation with numerous chart-topping hits",
+    "favorites": false
+  },
+  {
+    "id": "07",
+    "name": "Agnes Obel",
+    "birthdate": "1980-10-28",
+    "activeSince": "2010-present",
+    "genres": [
+      "Indie Folk",
+      "Chamber Pop",
+      "Alternative"
+    ],
+    "labels": [
+      "PIAS Recordings",
+      "Play It Again Sam"
+    ],
+    "website": "https://agnesobel.com/",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Agnes_Obel_live_in_2013.jpg/400px-Agnes_Obel_live_in_2013.jpg",
+    "roles": [
+      "Singer",
+      "Songwriter"
+    ],
+    "shortDescription": "Danish singer-songwriter known for ethereal and haunting music",
+    "favorites": true
+  }
+]
+  
+  
+  
+  
--- backend/server.js
+++ backend/server.js
@@ -65,3 +65,35 @@ app.delete('/artists/data/:id', async (req, res) => {
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
+
+
+______________________________
+
+
+// import express, { json } from "express";
+// import cors from "cors";
+// import fs from "fs/promises";
+
+// const app = express();
+// // using express and cors features
+// app.use(express.json());
+// app.use(cors());
+
+// app.get("/artists", async (request, response) => {
+//   const data = await fs.readFile("backend/data.json");
+//   const parsedData = await JSON.parse(data);
+//   response.json(parsedData);
+// });
+
+// //empty default responses
+// app.get("/", async (request, response) => {
+//   response.send("empty get request");
+// });
+// app.post("/", (request, response) => {
+//   response.send("empty post request");
+// });
+
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Serveren kører på: http://localhost:${port}`);
+// });
